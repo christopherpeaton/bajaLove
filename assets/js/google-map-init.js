7ea9cfee05db2bf18f4693878df526c31ee22e30
Christopher Eaton <christopherpeaton@gmail.com>
2016-06-21 06:34:27 -0700

    lots of changes
    changed all info on contact page, added main logo, went through all
    pages and fixed all broken links,added new dropdown menu in header for
    Get involved with trips, donate now, shop and stella and dot.

--- assets/js/google-map-init.js
+++ assets/js/google-map-init.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function() {
         var a = {
                 zoom: 11,
                 scrollwheel: false,
-                center: new google.maps.LatLng(40.67, -73.94),
+                center: new google.maps.LatLng(33.7422553,-118.0273566),
                 styles: [{
                     "featureType": "landscape",
                     "stylers": [
@@ -127,7 +127,7 @@ jQuery(document).ready(function() {
             b = document.getElementById("map"),
             c = new google.maps.Map(b, a);
         new google.maps.Marker({
-            position: new google.maps.LatLng(40.67, -73.94),
+            position: new google.maps.LatLng(33.7422553,-118.0273566),
             map: c,
             title: "Snazzy!"
         })
